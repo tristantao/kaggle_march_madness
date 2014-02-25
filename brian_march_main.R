7a2ae18704b1fa704f2853a2f03266c41b8bac28
Brian Wayne Liou <brian.liou91@gmail.com>
2014-02-25 11:47:49 -0800

    added analysis

--- brian_march_main.R
+++ brian_march_main.R
@@ -15,7 +15,7 @@ tourneySlots <- read.csv("data/tourney_slots.csv", header = TRUE, stringsAsFacto
 #  Wins by margin >= 7
 #  Losses by margin <= 3
 #  Losses by margin >= 7
-#  Num of wins in last 3 weeks or % of wins in last 3 weeks
+#  Num of wins in last 4 weeks or % of wins in last 4 weeks
 #  Num of wins in last 7 games
 #  Wins against teams that are in the tournament? (there are 304 teams in season A and 64 get into tourney)
 
@@ -46,9 +46,23 @@ homeWins_table <- as.data.frame(table(home_wins$wteam))
 homeWins_by_team <- homeWins_table[homeWins_table$Var1 %in% playoff_teams, ]
 
 neut_wins <- season_a[which(season_a$wloc == "N"), ]
-neutWins_table <- as.data.frame(table(neut_wins$wteam))
+neutWins_table <- as.data.frame(table(neut_wins$wteam),stringsAsFactors = FALSE)
 neutWins_by_team <- neutWins_table[neutWins_table$Var1 %in% playoff_teams, ]
 # SOME TEAMS had 0 Neutral games, namely teams: 539|551|625|721|701|662
+datone <- c(539, 0)
+dattwo <- c(551, 0)
+dathree <- c(625, 0)
+datfour <- c(721, 0)
+datfive <- c(701, 0)
+datsix <- c(662, 0)
+
+# Need to add these teams to the neutral wins table
+neutWins_by_team <- rbind(neutWins_by_team, datone,dattwo,dathree,datfour,datfive,datsix)
+neutWins_by_team_cor <- neutWins_by_team[order(neutWins_by_team$Var1), ]
+
+# Check to see calculations are correct
+wins_by_team$Freq #Should be the same as
+awayWins_by_team$Freq + homeWins_by_team$Freq + neutWins_by_team$Freq
 
 games_by_lt3 <- season_a[which(season_a$ptdiff <= 3), ]
 wins_lt3_table <- as.data.frame(table(games_by_lt3$wteam))
@@ -64,7 +78,44 @@ wins_gt7_by_team <- wins_gt7_table[wins_gt7_table$Var1 %in% playoff_teams, ]
 loss_gt7_table <- as.data.frame(table(games_by_gt7$wteam))
 loss_gt7_by_team <- loss_gt7_table[loss_gt7_table$Var1 %in% playoff_teams, ]
 
+#Figure out which row is daynum = 104 (4 weeks before last day daynum = 132)
+match(104, season_a$daynum)
+
+last_three_season_a <- season_a[match(104, season_a$daynum):nrow(season_a), ]
+last_three_win_table <- as.data.frame(table(last_three_season_a$wteam))
+last_three_win_by_team <- last_three_win_table[last_three_win_table$Var1 %in% playoff_teams, ]
+last_three_loss_table <- as.data.frame(table(last_three_season_a$lteam))
+last_three_loss_by_team <- last_three_loss_table[last_three_loss_table$Var1 %in% playoff_teams, ]
+
+#Need to write a utility function which adds back the missing playoff_team if they aren't
+#included in the statistic. For ex. Neutral Wins by Team. Always run this utility function
+#when calculating all of the statistics to make sure you have the data for all of the playoff teams
+#basically just adding c(teamID, 0) for when playoff_team %in% ________ is FALSE when utility function
+# != 64
+
+
+missingPteam <- function(statistic) {
+  vector <- playoff_teams %in% statistic$Var1
+  missTeam <- data.frame()
+  if (sum(vector) != 64) {
+    for(i in length(vector)) {
+      if (vector[i] == F) {
+        missTeam <- rbind(missTeam, c(playoff_teams[i], 0))
+      }
+    }
+    colnames(missTeam) <- c("Var1", "Freq")
+  }
+  fullTeam <- rbind(statistic, missTeam)
+  fullTeam <- fullTeam[order(fullTeam$Var1), ]
+  return(fullTeam)
+}
 ##Create matchup data in the same order as the submission order of the permutations of the
 ##teams will be
 
+testfunct <- function(stat) {
+  vector <- playoff_teams %in% stat$Var1
+  print(vector)
+}
 ## Should still be able to combine team data with matchup data to predict 0/1 for any matchup
+
+## WHat woud trends by team over multiple seasons loook like?
